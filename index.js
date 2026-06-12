2208477931d2a87456820bab474dfa62f6b2d2d5
a5825038 <a5825038@aoyama.ac.jp>
2026-06-12 17:10:47 +0900

    todotitle

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ let tasks = [
  */
 function addTask(title) {
     const task = { isDone: false }
-
     // やることのタイトルを設定(このままだと、すべてのタイトルがHello Worldになってしまう！)
-    task.title = "Hello World"
+    task.title = title;
 
     // 以降は無視して良い
     tasks.push(task);
@@ -44,6 +43,7 @@ function onUpdateIsDone(task) {
 
     // 達成状態を更新(このままだと、現在の達成状態で更新されてしまう！)
     const currentValue = task.isDone;
+    
     task.isDone = currentValue;
 
     return task;
